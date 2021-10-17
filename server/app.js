0e86015333f826dd98ccdb76b52fee3d9fc0998d
dnr14 <dnr14@naver.com>
2021-10-18 08:30:00 +0900

    [feat] 회원가입 api 개발

--- server/app.js
+++ server/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const logger = require("./middleware/logger");
+const db = require("./db");
 const app = express();
 const PORT = process.env.PORT || 5000;
-
+db();
 app.use(logger());
+app.use(express.json());
 app.use("/api/users", require("./routers/users"));
 app.use("*", (_, res) => res.send("404 not found"));
 
