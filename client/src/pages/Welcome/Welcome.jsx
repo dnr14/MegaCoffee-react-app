008aba9588dd48a9f645e9bf3d10c18ef30e7fc1
dnr14 <dnr14@naver.com>
2021-10-19 16:44:18 +0900

    [feat] 회원가입 환영 페이지 개발

---
+++ client/src/pages/Welcome/Welcome.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect } from 'react';
+import { Link, useLocation, useHistory } from 'react-router-dom';
+import styled from 'styled-components';
+import Button from '@/components/Button';
+import Title from '@/components/Title';
+import Layout from '@/components/Layout';
+
+const Welcome = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  useEffect(() => {
+    const { state } = location;
+    if (!state) {
+      history.push('/membership');
+    }
+  }, [location, history]);
+
+  return (
+    <Layout>
+      <Title>🎉 환영 합니다.</Title>
+      <StyledDid>
+        <div>{location.state.props.id}님 가입을 축하드립니다.</div>
+        <Link
+          to={{
+            pathname: '/login',
+            props: {
+              id: location.state.props.id,
+            },
+          }}
+        >
+          <Button>sign in</Button>
+        </Link>
+      </StyledDid>
+    </Layout>
+  );
+};
+
+const StyledDid = styled.div`
+  margin-bottom: 2rem;
+
+  & > div {
+    padding: 2rem 0;
+    text-align: center;
+    word-break: keep-all;
+    line-height: 1.5rem;
+  }
+`;
+
+export default Welcome;
