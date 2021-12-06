11cb8062a1d55b3ef5bbe1cc2186d75676426126
Minwook Lee <dnr14@naver.com>
2021-12-06 22:21:59 +0900

    [feat] server cors 추가

--- server/app.js
+++ server/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const logger = require("./middleware/logger");
 const path = require("path");
 const db = require("./db");
+const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 const ROOT = path.join(__dirname, "/public");
@@ -10,6 +11,17 @@ const IMG_PATH = `${ROOT}/files/profile/`;
 db();
 app.use(logger());
 app.use(express.json());
+app.use(cors());
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, PATCH");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, X_BPI_CONTEXT"
+  );
+  res.header("Content-Type", "application/json");
+  next();
+});
 app.use("/api/users", require("./routers/users"));
 app.use("/api/auth", require("./routers/auth"));
 app.use("/api/admin", require("./routers/admin"));
